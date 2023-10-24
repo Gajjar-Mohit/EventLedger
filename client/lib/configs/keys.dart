4572c13bf03f1ce0d15e45d81bd8b412f0684bee
Mohit Gajjar <mohit@Mohits-Macbook.local>
2023-10-24 10:39:43 +0530

    Test-Deploy-Push

--- client/lib/configs/keys.dart
+++ client/lib/configs/keys.dart
@@ -2,11 +2,13 @@ const String ipfsApiKey =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweGJBMDk5OUREYjhkYjI1QTRCRGJCYWY3RTdlMkJjMGE3QjA2YzgwNjkiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY5MzA0NTkyMzI1OCwibmFtZSI6IkV2ZW50TGVkZ2VyIn0.pjyczAXmKVRuqdzn7TWqB4mXgumSZeU1zPHr-xuaqWI";
 const String rpcUrl =
     "https://polygon-mumbai.g.alchemy.com/v2/_545cXZb3whqPZ9eLyvHxT1h771CJGx1";
-const ipfsURL = 'https://ipfs.io/ipfs/';
+const ipfsURL = 'https://ipfs.io/ipfs/bafybeifvvk53v6mrfuiakx662jorl6ecbjivyjrcatlt6wbjiba7i62jbe';
+// const ipfsURL =
+//     'https://ipfs.io/ipfs/bafybeia3tcg4mpybymrlxcho4upz6pmbdqadpkwhhlx3ylql76rehmd7lq';
 const chainId = 80001;
 // const chainId = 1337;
 
 // const String rpcUrl =
 //     "HTTP://0.0.0.0:7545";
 
-// 735e5044c8387270cdabf8efcc7205746e4e9d9af30dec9d530cc2916fd93b6e
+// 6fb3226e632312a7c93397d234f2049fe865b9757ef1aceb0edc545b1b75f027
--- client/lib/pages/issuer/home.dart
+++ client/lib/pages/issuer/home.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:client/pages/issuer/onboarding.dart';
-import 'package:client/providers/wallet_provider.dart';
 import 'package:client/services/contract_service.dart';
 import 'package:client/services/ipfs_service.dart';
 import 'package:client/services/wallet_service.dart';
@@ -21,6 +20,7 @@ class IssuerHome extends StatefulWidget {
 
 class _IssuerHomeState extends State<IssuerHome> {
   WalletService walletService = WalletService();
+  bool isLoading = false;
   void logout() {
     walletService.removePrivateKey().then((value) => Navigator.pushReplacement(
         context, MaterialPageRoute(builder: (context) => const Onboarding())));
@@ -31,10 +31,10 @@ class _IssuerHomeState extends State<IssuerHome> {
     // print(wallet.accountBalance);
     // contactService
     //     .registerEventFunction(
-    //         "Spoural 2023",
+    //         "Hack This Fall",
     //         "21/JAN/2023",
     //         EthereumAddress.fromHex(
-    //             '0x1cf847fefd7858e8e4d226f234dfd4fcdeb98c3d'))
+    //             '0xab3561c8a866e5c05a3efe14a2958c16c5a5f76b'))
     //     .then((value) {
     //   print("Registered");
     // });
@@ -112,16 +112,22 @@ class _IssuerHomeState extends State<IssuerHome> {
 
   IpfsService ipfsService = IpfsService();
   void captureCertificatePNG() {
+
     ContractService contactService =
         Provider.of<ContractService>(context, listen: false);
     screenshotController.capture().then((image) {
       ipfsService.uploadImageWeb(image!).then((cid) {
         contactService.generateCertificateFunction(cid).then((value) {
+          setState(() {
+            isLoading = false;
+          });
           Fluttertoast.showToast(msg: "Certificate Uploaded");
         });
       });
     }).catchError((onError) {
       print(onError);
+    
+      // Fluttertoast.showToast(msg: onError);
     });
   }
 
@@ -177,144 +183,155 @@ class _IssuerHomeState extends State<IssuerHome> {
           ),
         ],
       ),
-      body: Screenshot(
-        controller: screenshotController,
-        child: Container(
-            width: 1920,
-            margin: const EdgeInsets.all(20),
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(22),
-                color: const Color.fromARGB(255, 231, 234, 239)),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    const Text(
-                      "Certificate",
-                      style: TextStyle(
-                          fontSize: 90, height: 1, fontWeight: FontWeight.bold),
-                    ),
-                    const Text(
-                      "of Appreciation",
-                      style: TextStyle(
-                          fontSize: 50, height: 1, fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    const Text(
-                      "This certificate is awareded to",
-                      style: TextStyle(
-                          fontSize: 20, height: 1, fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    InkWell(
-                      onTap: () {
-                        enterNameDialog(context);
-                      },
-                      child: Text(
-                        name,
-                        style: const TextStyle(
-                            fontSize: 50, fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width / 2,
-                      child: RichText(
-                        textAlign: TextAlign.end,
-                        text: TextSpan(
-                          children: [
-                            const TextSpan(
-                              text:
-                                  'In recognition of their dedication and contribution to the event, we hereby present this certificate as a token of appreciation on ',
-                              style: TextStyle(),
-                            ),
-                            TextSpan(
-                              recognizer: TapGestureRecognizer()
-                                ..onTap = () {
-                                  enterPrizeDialog(context);
-                                },
-                              text: prize,
+      body: isLoading
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : Screenshot(
+              controller: screenshotController,
+              child: Container(
+                  width: 1920,
+                  margin: const EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(22),
+                      color: const Color.fromARGB(255, 231, 234, 239)),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          const Text(
+                            "Certificate",
+                            style: TextStyle(
+                                fontSize: 90,
+                                height: 1,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          const Text(
+                            "of Appreciation",
+                            style: TextStyle(
+                                fontSize: 50,
+                                height: 1,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(
+                            height: 30,
+                          ),
+                          const Text(
+                            "This certificate is awareded to",
+                            style: TextStyle(
+                                fontSize: 20,
+                                height: 1,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(
+                            height: 30,
+                          ),
+                          InkWell(
+                            onTap: () {
+                              enterNameDialog(context);
+                            },
+                            child: Text(
+                              name,
                               style: const TextStyle(
-                                color: Color(0xFF4B39EF),
-                                fontSize: 16,
-                                fontWeight: FontWeight.w600,
+                                  fontSize: 50, fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width / 2,
+                            child: RichText(
+                              textAlign: TextAlign.end,
+                              text: TextSpan(
+                                children: [
+                                  const TextSpan(
+                                    text:
+                                        'In recognition of their dedication and contribution to the event, we hereby present this certificate as a token of appreciation on ',
+                                    style: TextStyle(),
+                                  ),
+                                  TextSpan(
+                                    recognizer: TapGestureRecognizer()
+                                      ..onTap = () {
+                                        enterPrizeDialog(context);
+                                      },
+                                    text: prize,
+                                    style: const TextStyle(
+                                      color: Color(0xFF4B39EF),
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                  const TextSpan(
+                                    text: 'in ',
+                                    style: TextStyle(),
+                                  ),
+                                  TextSpan(
+                                    text: "$eventName ",
+                                    style: const TextStyle(
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  TextSpan(
+                                    text: 'held on $eventDate.',
+                                    style: const TextStyle(),
+                                  ),
+                                ],
+                                style: const TextStyle(
+                                  color: Color(0xFF57636C),
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w500,
+                                ),
                               ),
                             ),
-                            const TextSpan(
-                              text: 'in ',
-                              style: TextStyle(),
-                            ),
-                            TextSpan(
-                              text: "$eventName ",
-                              style:
-                                  const TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                            TextSpan(
-                              text: 'held on $eventDate.',
-                              style: const TextStyle(),
-                            ),
-                          ],
-                          style: const TextStyle(
-                            color: Color(0xFF57636C),
-                            fontSize: 16,
-                            fontWeight: FontWeight.w500,
                           ),
-                        ),
+                          const SizedBox(
+                            height: 70,
+                          ),
+                          Row(
+                            children: [
+                              const Column(
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                children: [
+                                  Text("Certificate is Verified by"),
+                                  Text(
+                                    "EventLedger",
+                                    style: TextStyle(
+                                        fontSize: 30,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              Image.asset(
+                                'assets/verified.png',
+                                height: 100,
+                              )
+                            ],
+                          )
+                        ],
                       ),
-                    ),
-                    const SizedBox(
-                      height: 70,
-                    ),
-                    Row(
-                      children: [
-                        const Column(
-                          crossAxisAlignment: CrossAxisAlignment.end,
-                          children: [
-                            Text("Certificate is Verified by"),
-                            Text(
-                              "EventLedger",
-                              style: TextStyle(
-                                  fontSize: 30, fontWeight: FontWeight.bold),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(
-                          width: 10,
-                        ),
-                        Image.asset(
-                          'assets/verified.png',
-                          height: 100,
-                        )
-                      ],
-                    )
-                  ],
-                ),
-                const SizedBox(
-                  width: 40,
-                ),
-                Container(
-                  margin: const EdgeInsets.all(10),
-                  width: MediaQuery.of(context).size.width / 4,
-                  height: MediaQuery.of(context).size.height,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(18),
-                      image: const DecorationImage(
-                          fit: BoxFit.fitHeight,
-                          image: AssetImage("assets/image.jpeg"))),
-                )
-              ],
-            )),
-      ),
+                      const SizedBox(
+                        width: 40,
+                      ),
+                      Container(
+                        margin: const EdgeInsets.all(10),
+                        width: MediaQuery.of(context).size.width / 4,
+                        height: MediaQuery.of(context).size.height,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(18),
+                            image: const DecorationImage(
+                                fit: BoxFit.fitHeight,
+                                image: AssetImage("assets/image.jpeg"))),
+                      )
+                    ],
+                  )),
+            ),
     );
   }
 }
